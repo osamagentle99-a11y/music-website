500e7bab2a02d5a8134873963c2bfae13b7e738a
Osama Ali <osamagentle99@gmail.com>
2026-08-26 12:16:11 +0500

    Update server port for deployment

--- server/index.js
+++ server/index.js
@@ -39,5 +39,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on `);
 });
