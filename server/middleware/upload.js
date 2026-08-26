bc30cf481cfc477d25e6e858266205fff37c1a6b
Osama Ali <osamagentle99@gmail.com>
2026-08-26 20:35:28 +0500

    Fix multer upload paths for Render

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,21 +1,39 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// server folder
+const serverRoot = path.join(__dirname, "..");
+
+// Upload folders
+const imagePath = path.join(serverRoot, "uploads", "images");
+const audioPath = path.join(serverRoot, "uploads", "songs");
+
+// Folders automatically create ho jayenge
+fs.mkdirSync(imagePath, { recursive: true });
+fs.mkdirSync(audioPath, { recursive: true });
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-
     if (file.fieldname === "image") {
-      cb(null, "uploads/images");
+      cb(null, imagePath);
     } else if (file.fieldname === "audio") {
-      cb(null, "uploads/songs");
+      cb(null, audioPath);
+    } else {
+      cb(new Error("Invalid file field"));
     }
-
   },
 
   filename: (req, file, cb) => {
     cb(
       null,
-      Date.now() + "-" + file.originalname.replace(/\s+/g, "_")
+      Date.now() +
+        "-" +
+        file.originalname.replace(/\s+/g, "_")
     );
   },
 });
